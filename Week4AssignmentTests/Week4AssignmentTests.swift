24136efd21663646bbc8d7ea61bdba6fb1a72957
waiyanphyohein <waiyanphyohein@gmail.com>
2018-10-20 15:53:11 -0400

    Initial Commit

---
+++ Week4AssignmentTests/Week4AssignmentTests.swift
@@ -0,0 +1,34 @@
+//
+//  Week4AssignmentTests.swift
+//  Week4AssignmentTests
+//
+//  Created by WaiYanPhyo Hein on 10/20/18.
+//  Copyright © 2018 WaiYanPhyo Hein. All rights reserved.
+//
+
+import XCTest
+@testable import Week4Assignment
+
+class Week4AssignmentTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
